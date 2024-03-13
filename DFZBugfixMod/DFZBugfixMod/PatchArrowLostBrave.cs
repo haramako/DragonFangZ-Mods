d1d36d5106a7c9da0179c473a8204649f2b23e41
HARADA Makoto <haramako@gmail.com>
2024-03-13 19:23:02 +0900

    Fix: Lost brave when arrow hits with max brave.

---
+++ DFZBugfixMod/DFZBugfixMod/PatchArrowLostBrave.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Game;
+using Master;
+using Game.Thinkings;
+
+namespace DFZBugfixMod
+{
+    public partial class DFZBugrfixMod : MelonMod
+    {
+        /// <summary>
+        /// ブレイブMAX時に、弓矢が当たった時にブレイブを維持できない問題の修正.
+        /// </summary>
+        [HarmonyPatch(typeof(Game.G), "loadOther")]
+        public static class PatchArrowLostBrave
+        {
+            public static void Postfix()
+            {
+                // 弓矢のスキル(ID=5)を変更する
+                var s = G.FindSkillById(5);
+                s.Codes[0].Specials[0].Attribute = SpecialAttribute.ArrowAttack; // SpecialAttribute.MultiAttackを消す
+            }
+        }
+    }
+}
+
